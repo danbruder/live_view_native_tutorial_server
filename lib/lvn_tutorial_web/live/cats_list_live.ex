8582da651519516e058f14087dc345608b035de5
Dan Bruder <danb@structionsite.com>
2022-09-04 15:14:31 -0400

    again

--- lib/lvn_tutorial_web/live/cats_list_live.ex
+++ lib/lvn_tutorial_web/live/cats_list_live.ex
@@ -46,7 +46,7 @@ defmodule LvnTutorialWeb.CatsListLive do
 
   def handle_event("toggle-favorite", %{"name" => name}, socket) do
     FavoritesStore.toggle_favorite(name)
-    new = get_cats_and_favorites
+    new = get_cats_and_favorites()
     {:noreply, assign(socket, cats_and_favorites: new)}
   end
 end
